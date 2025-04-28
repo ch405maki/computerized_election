fbe4e5823a4498ec8081b576ea21c59321a21d41
Mac Paul Custodio <custodiomacpaul@gmail.com>
2025-04-28 15:41:24 +0800

    Update Show.vue

--- resources/js/pages/Reports/Results/Show.vue
+++ resources/js/pages/Reports/Results/Show.vue
@@ -26,7 +26,7 @@ const props = defineProps<{
 }>();
 
 const breadcrumbs = computed<BreadcrumbItem[]>(() => [
-  { title: 'Results History', href: '/results' },
+  { title: 'Results History', href: '/reports/results' },
   { title: props.election?.name || 'Election Results', href: route('results.show', props.election?.id) },
 ]);
 
