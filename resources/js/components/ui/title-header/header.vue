2fa0d0026e5500a5f667087256245f3b30f81ad6
ch405maki <markmanuel0317@gmail.com>
2026-01-29 16:07:30 +0800

    working on ui

---
+++ resources/js/components/ui/title-header/header.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+            <h1 class="text-xl font-bold">Candidates List</h1>
+          <p class="text-sm test-muted-foreground ">This is list of candidates</p>
+          </div>
+</template>
