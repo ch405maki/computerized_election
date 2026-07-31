465f43f4824945596f55531a92da833d24b4911f
Mac Paul Custodio <custodiomacpaul@gmail.com>
2026-07-31 13:55:22 +0800

    Update AppSidebar.vue

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -64,7 +64,7 @@ const baseMainNavItems = ref<DropdownNavItem[]>([
 ]);
 
 const baseConfigNavItems = ref<DropdownNavItem[]>([
-    { title: 'Election', href: '/election', icon: Cog, permissionKey: 'showElectionTab' },
+    { title: 'Election', href: '/elections', icon: Cog, permissionKey: 'showElectionTab' },
     {
         title: 'User Management', href: '#', icon: UserRoundCog, isOpen: false,
         children: [{ title: 'Users', href: '/users', icon: UsersRound }],
